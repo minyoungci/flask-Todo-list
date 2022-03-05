017c9576a81d8bf18f4d7567c366a2a00012717c
min <80457917+minyoungci@users.noreply.github.com>
2022-03-05 18:22:02 +0900

    css 수정

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ def index():
             return "There was an issue adding your task"
         
     else:
-        return render_template('index.html')
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
