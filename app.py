5f2519645a740dd01e8d4128ce6201de7acfa0ff
min <80457917+minyoungci@users.noreply.github.com>
2022-03-05 15:50:03 +0900

    first flask

---
+++ app.py
@@ -0,0 +1,36 @@
+from pickletools import read_unicodestring1
+from flask import Flask, redirect, render_template, request, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# Flask 객체 인스턴스 생성 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST','GET']) # 접속하는 url 
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue adding your task"
+        
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
