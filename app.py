3d27827a8aa717f6f3562d2fbea576a602a8fd80
min <80457917+minyoungci@users.noreply.github.com>
2022-03-06 13:42:18 +0900

    app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 # Flask 객체 인스턴스 생성 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
@@ -33,6 +34,31 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
     
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that task "
+    
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "there was an issue updating your task"
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
